faa21ff75fbe125e86601f4bf35818278e1f512e
Adib Rastegarnia <arastega@purdue.edu>
2016-01-03 19:42:19 -0500

    updated

--- graph/toposort.py
+++ graph/toposort.py
@@ -76,12 +76,18 @@ def main():
 
     """
     ## 5 4 2 3 1 0
+    """
     graph.add_edge(5,2)
     graph.add_edge(5,0)
     graph.add_edge(4,0)
     graph.add_edge(4,1)
     graph.add_edge(2,3)
     graph.add_edge(3,1)
+    """
+    graph.add_edge(1,2)
+    graph.add_edge(2,3)
+    graph.add_edge(3,4)
+    graph.add_edge(4,5)
     vertices = graph.vertices()
     
     for v in vertices:
